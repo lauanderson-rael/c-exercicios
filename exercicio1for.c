c7754f24e2f79f817ba57b8aca8e4002561b4b0d
Lauanderson Rael <lauanderson38@gmail.com>
2024-03-05 09:22:12 -0300

    salvado alteracoes finais

--- exercicio1for.c
+++ exercicio1for.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 void main(){
 
-    float n1, n2, dif, somaPos, somaNeg, qtdPos, qtdNeg;
-    somaPos = 0; somaNeg = 0;
+    float n1, n2, dif, somaPos, somaNeg;
+    int qtdPos, qtdNeg;
+    somaPos = 0; somaNeg = 0.0;
     qtdPos = 0;  qtdNeg = 0;
 
     printf("Digite um numero: ");
@@ -32,17 +33,20 @@ void main(){
             }
 
         } else { 
-            printf("Erro, o intervalo minimo entre os valores deve ser 10!");
+            printf("Erro, o intervalo minimo entre os valores deve ser 10!\n");
             }
+            exit(0);
     }
     
     else if (n1 < n2){
         dif = n2 - n1;
         if (dif >= 10){
             n1+=1;
+            printf("\nNumeros do intervalo: \n");
             for (n1; n1 < n2; n1++ ){
+                printf("%1.f ", n1);
                 if (n1>0){
-                    somaPos+=n2;
+                    somaPos+=n1;
                     qtdPos+=1;
                 }
                 else if(n1 < 0){
@@ -53,11 +57,19 @@ void main(){
             }
 
         } else { 
-            printf("Erro, o intervalo minimo entre os valores deve ser 10!");
+            printf("Erro, o intervalo minimo entre os valores deve ser 10!\n");
             }
+            exit(0);
+            
     }
     
-    printf("\n\nMedia dos positivos: %.2f\n", (somaPos/qtdPos));
-    printf("Media dos negativos: %.2f\n", (somaNeg/qtdNeg));
+    float mediaP = somaPos/qtdPos;
+    float mediaN = somaNeg/qtdNeg;
+    printf("\n\nMedia dos positivos: %.2f\n", mediaP);
+    if (qtdNeg != 0){
+        printf("Media dos negativos: %.2f\n", mediaN);
+    }else{
+        printf("Media dos negativos: 0.0 ");
+    }
 
 }
